1d51b7220350d6eeeca028f50b1298619285d7df
bijianpeng <bijianpeng@126.com>
2019-04-28 00:01:05 +0800

    commit

--- helloworld-cxf/src/main/java/com/bjp/helloworld/cxf/HelloServiceImpl.java
+++ helloworld-cxf/src/main/java/com/bjp/helloworld/cxf/HelloServiceImpl.java
@@ -4,7 +4,7 @@ package com.bjp.helloworld.cxf;
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "com.bjp.helloworld.cxf.HelloService")
-public class HelloServiceImpl implements HelloService{
+public class HelloServiceImpl implements HelloService {
     public String sayHello(String name) {
         return "Hello," + name;
     }
